d52cd4fec9c5408f8962a11425dc384ea0339ffa
Andrew Seward <awswrd@gmail.com>
2021-10-11 19:34:56 -0700

    Add a test for `prune` in `libdeploy.sh`

--- test/default.sh
+++ test/default.sh
@@ -6,3 +6,19 @@ set -euo pipefail
 # working directory should be that.
 
 ./example.sh
+
+( # Test `prune` in `libdeploy.sh`
+
+  set -euo pipefail
+
+  # shellcheck disable=SC1091
+  . templates/libdeploy.sh
+  tmp_dir="$(mktemp -d)"; readonly tmp_dir
+  cd "${tmp_dir}" || exit 1
+
+  touch foo
+  touch bar
+  touch baz
+
+  prune '.' 2 && ls -lah
+)
